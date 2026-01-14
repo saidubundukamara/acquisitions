d264145b4bdb38dc805f1b2efe98eb2ee5e303a0
saidubundukamara <bundukamarasaidu@gmail.com>
2026-01-14 01:13:19 +0000

    Added logger and middlewares

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,20 +1,17 @@
 import winston from 'winston';
+import 'dotenv/config';
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine((
+    winston.format.timestamp(),
+    winston.format.errors({stack: true}),
+    winston.format.json()
+  )),
+  defaultMeta: { service: 'acquisations-api' },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or higher to `error.log`
-    //   (i.e., error, fatal, but not other levels)
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    //
-    // - Write all logs with importance level of `info` or higher to `combined.log`
-    //   (i.e., fatal, error, warn, and info, but not trace)
-    //
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
@@ -25,7 +22,12 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: winston.format.combine((
+        winston.format.colorize(),
+        winston.format.simple()
+      )),
     })
   );
 }
+
+export default logger;
